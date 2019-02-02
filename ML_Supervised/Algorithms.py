24157c9a176ee6af4a6b147f89b1d8e3df22a823
Cooper Ramsey <32044877+cooperdramsey@users.noreply.github.com>
2019-02-02 15:13:31 -0700

    Add all classifiers with cross validation

--- ML_Supervised/Algorithms.py
+++ ML_Supervised/Algorithms.py
@@ -10,37 +10,70 @@ from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_sc
 
 
 class Algorithms:
-    def __init__(self, data_path):
+    def __init__(self, data_path, test_split):
         self.data = self.load_data(data_path)
+        self.test_split = test_split
 
     def decision_tree(self, parameters):
         algorithm = DecisionTreeClassifier()
-        clf = GridSearchCV(algorithm, parameters, cv=5)
-        X_train, X_test, y_train, y_test = self.split_data(0.2)
+        clf = GridSearchCV(algorithm, parameters, cv=5, iid=False)
+        X_train, X_test, y_train, y_test = self.split_data(self.test_split)
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
-        print("Decision Tree: " + str(self.get_accuracy(y_test, y_pred)) + "%")
+        scores = self.get_scores(y_test, y_pred)
+        print("Decision Tree: ",
+              "Acc: " + str(scores['acc'] * 100) + "%")
 
     def neural_network(self, parameters):
         algorithm = MLPClassifier()
-        clf = GridSearchCV(algorithm, parameters, cv=5)
-        X_train, X_test, y_train, y_test = self.split_data(0.2)
+        clf = GridSearchCV(algorithm, parameters, cv=5, iid=False)
+        X_train, X_test, y_train, y_test = self.split_data(self.test_split)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        scores = self.get_scores(y_test, y_pred)
+        print("Neural Net: ",
+              "Acc: " + str(scores['acc'] * 100) + "%")
 
     def tree_boosting(self, parameters):
         # uses decision tree as the base algorithm by default
         algorithm = AdaBoostClassifier()
-        clf = GridSearchCV(algorithm, parameters, cv=5)
-        X_train, X_test, y_train, y_test = self.split_data(0.2)
+        clf = GridSearchCV(algorithm, parameters, cv=5, iid=False)
+        X_train, X_test, y_train, y_test = self.split_data(self.test_split)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        scores = self.get_scores(y_test, y_pred)
+        print("AdaBoost:",
+              "Acc: " + str(scores['acc'] * 100) + "%")
 
     def support_vector_machine(self, parameters):
         algorithm = SVC()
-        clf = GridSearchCV(algorithm, parameters, cv=5)
-        X_train, X_test, y_train, y_test = self.split_data(0.2)
+        clf = GridSearchCV(algorithm, parameters, cv=5, iid=False)
+        X_train, X_test, y_train, y_test = self.split_data(self.test_split)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        scores = self.get_scores(y_test, y_pred)
+        print("SVM:",
+              "Acc: " + str(scores['acc'] * 100) + "%")
+
+    def linear_support_vector_machine(self, parameters):
+        algorithm = SVC()
+        clf = GridSearchCV(algorithm, parameters, cv=5, iid=False)
+        X_train, X_test, y_train, y_test = self.split_data(self.test_split)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        scores = self.get_scores(y_test, y_pred)
+        print("Linear SVM:",
+              "Acc: " + str(scores['acc'] * 100) + "%")
 
     def k_nearest_neighbors(self, parameters):
         algorithm = KNeighborsClassifier()
-        clf = GridSearchCV(algorithm, parameters, cv=5)
-        X_train, X_test, y_train, y_test = self.split_data(0.2)
+        clf = GridSearchCV(algorithm, parameters, cv=5, iid=False)
+        X_train, X_test, y_train, y_test = self.split_data(self.test_split)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        scores = self.get_scores(y_test, y_pred)
+        print("KNN:",
+              "Acc: " + str(scores['acc'] * 100) + "%")
 
     def load_data(self, data_path):
         return pd.read_csv(data_path)
@@ -48,8 +81,16 @@ class Algorithms:
     def show_data(self):
         print(self.data.head())
 
-    def get_accuracy(self, y_true, y_pred):
-        return accuracy_score(y_true, y_pred)
+    def get_scores(self, y_true, y_pred):
+        acc = accuracy_score(y_true, y_pred)
+        #prec = precision_score(y_true, y_pred)
+        #recall = recall_score(y_true, y_pred)
+        #f1 = f1_score(y_true, y_pred)
+        return {'acc': acc
+                #'prec': prec,
+                #'recall': recall,
+                #'f1': f1
+                }
 
     def split_data(self, test_size):
         y = self.data.iloc[:, -1]
--- ML_Supervised/main.py
+++ ML_Supervised/main.py
@@ -1,47 +1,113 @@
 from Algorithms import Algorithms
+from timeit import default_timer as timer
+
+# init
+param_grids = {'tree':
+                   [{'criterion': ['gini', 'entropy'],
+                     'splitter': ['best', 'random'],
+                     'min_samples_leaf':[1, 2, 4, 5, 6],
+                     'min_samples_split': [2]
+                     }],
+               'neural': [{'hidden_layer_sizes': [(100,)],
+                           'activation': ['relu'], #'identity', 'logistic', 'tanh',
+                           'alpha': [0.0001],
+                           'learning_rate': ['constant'], # 'constant', 'invscaling', 'adaptive'
+                           'learning_rate_init': [0.001],
+                           'max_iter': [2000],
+                           }],
+               'boost': [{'n_estimators': [50, 25],
+                          'learning_rate': [1, 0.5]
+                          }],
+               'svm': [{'C': [1],  # just add zeros
+                        'kernel': ['linear'], # rbf
+                        'gamma': ['auto'] # 0.0001
+                        }],
+               'knn': [{'n_neighbors': [5, 8, 10]}],
+               'linearSVM': [{'C': [1],
+                              'gamma': ['scale']}]
+               }
+test_split = 0.4
 
 # Data set 1
 data_path_1 = 'winequality-red.csv'
-tests_1 = Algorithms(data_path_1)
-param_grids = {'tree':
-                   [{'criterion': ['gini', 'entropy'], 'splitter': ['best', 'random'], 'min_samples_leaf':[2, 4, 5, 6]}],
-               'neural': [],
-               'boost': [],
-               'svm': [],
-               'knn': []
-               }
+tests_1 = Algorithms(data_path_1, test_split)
 
+print("DATA SET 1")
 # Decision tree
+start = timer()
 tests_1.decision_tree(param_grids['tree'])
+end = timer()
+print(end - start)
 
 # neural network
+start = timer()
 tests_1.neural_network(param_grids['neural'])
+end = timer()
+print(end - start)
 
 # Boosting
+start = timer()
 tests_1.tree_boosting(param_grids['boost'])
-
-# SVM
-tests_1.support_vector_machine(param_grids['svm'])
+end = timer()
+print(end - start)
 
 # KNN
+start = timer()
 tests_1.k_nearest_neighbors(param_grids['knn'])
+end = timer()
+print(end - start)
+
+# Linear SVM
+start = timer()
+tests_1.linear_support_vector_machine(param_grids['linearSVM'])
+end = timer()
+print(end - start)
+
+# SVM
+start = timer()
+tests_1.support_vector_machine(param_grids['svm'])
+end = timer()
+print(end - start)
 
 ####################################################################################
+print()
+print('DATA SET 2')
 # Data set 2
 data_path_2 = 'UCI_Credit_Card.csv'
-tests_2 = Algorithms(data_path_2)
+tests_2 = Algorithms(data_path_2, test_split)
 
-# Decision Tree
+# Decision tree
+start = timer()
 tests_2.decision_tree(param_grids['tree'])
+end = timer()
+print(end - start)
 
 # neural network
+start = timer()
 tests_2.neural_network(param_grids['neural'])
+end = timer()
+print(end - start)
 
 # Boosting
+start = timer()
 tests_2.tree_boosting(param_grids['boost'])
-
-# SVM
-tests_2.support_vector_machine(param_grids['svm'])
+end = timer()
+print(end - start)
 
 # KNN
+start = timer()
 tests_2.k_nearest_neighbors(param_grids['knn'])
+end = timer()
+print(end - start)
+
+# # Linear SVM
+# start = timer()
+# tests_2.linear_support_vector_machine(param_grids['linearSVM'])
+# end = timer()
+# print(end - start)
+#
+# # SVM
+# start = timer()
+# tests_2.support_vector_machine(param_grids['svm'])
+# end = timer()
+# print(end - start)
